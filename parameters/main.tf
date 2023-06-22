eb241b34c2926792b6cbd69b212db540fef54b08
sarithadevops23@gmail.com <sarithadevops23@gmail.com>
2023-06-22 22:18:59 +0530

    int commit

---
+++ parameters/main.tf
@@ -0,0 +1,18 @@
+resource "aws_ssm_parameter" "params" {
+  for_each = var.params
+  name  = each.value.name
+  type  = each.value.type
+  value = each.value.value
+}
+
+variable "params"{
+
+	default={
+		parameters={
+			name = "demo1", type=String, value = "demo1"
+			name = "demo2", type=String, value = "demo2"
+
+		}
+
+	}
+}
