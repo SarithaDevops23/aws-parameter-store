ff5f9933aa9d6ded0aac067935d8b0d3c94f0f06
sarithadevops23@gmail.com <sarithadevops23@gmail.com>
2023-06-25 12:35:30 +0530

    url update

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ resource "aws_ssm_parameter" "params" {
 variable "params"{
 
 	default=[
-			{name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.devops23.store", type="String"},
-			{name = "roboshop.dev.frontend.user_url", value = "http://user-dev.devops23.store", type="String"},
-			{name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.devops23.store", type="String"},
-			{name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.devops23.store", type="String"},
-			{name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.devops23.store", type="String"},
+			{name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.devops23.store:8080/", type="String"},
+			{name = "roboshop.dev.frontend.user_url", value = "http://user-dev.devops23.store:8080/", type="String"},
+			{name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.devops23.store:8080/", type="String"},
+			{name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.devops23.store:8080/", type="String"},
+			{name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.devops23.store:8080/", type="String"}
 			
 
 ]
